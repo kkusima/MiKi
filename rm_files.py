cc975d5d045911b8409b1d90e761fe22ca67d35d
Kenneth Kusima <32442248+kkusima@users.noreply.github.com>
2023-06-08 21:58:52 -0500

    06_08_fxed duplicates

---
+++ rm_files.py
@@ -0,0 +1,11 @@
+import os, glob
+dirs = glob.glob('*.*')
+dirs.sort()
+
+cwd = os.getcwd()
+
+for d in dirs:
+    if os.path.isdir(d):
+        os.chdir(d)
+        os.system('rm *\ 2.*')
+        os.chdir(cwd)
